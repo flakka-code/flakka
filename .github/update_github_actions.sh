ce7c399262cbedd5b430a66f8a560a2f4985ecc9
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-01-14 21:52:18 -0600

    chore: try mono_repo

--- .github/update_github_actions.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-mason make github_actions_dart --on-conflict overwrite --exclude '' --minCoverage 140 --flutterVersion '3.13.6' --flutterChannel stable --dartChannel stable --dependabotFrequency daily --generateDependabot true --generateSemanticPullRequest true --generateSpellCheck false --spellCheckConfig cspell.json --workflowRef main
